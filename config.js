3fcef060cc9bbdcd943d65d1e7c38f96e20658bd
lenny <cocoblackett@gmail.com>
2025-05-07 12:54:16 +0200

    config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: '0AAP72QQ0ET59N7TRTPA', // TODO
+    secretAccessKey: 'i4XDbFOBRXrkEl4OI00GakEBUROHRtlSoCMsmfZZ', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-86672', // TODO
   },
   suffix: {
     small: '_small.jpg',
